d837033b973223db16eb7426860946ca169694fe
breus32 <muller.09@mail.ru>
2016-05-05 21:41:55 +0300

    Update zavdannya1.c

--- zavdannya1.c
+++ zavdannya1.c
@@ -1,27 +1,39 @@
 #include <stdio.h>
 
+int max (float a, float b, float c)
+
+{
+
+   if ((a > b) && (a > c)) {
+   return a;
+   } else {
+   	if ((b > a) && (b > c)){
+    		return b;
+    	} else {
+    		return c;
+    	}
+	
+    return b;
+    
+    }
+
 int main ()
 {
-	int a, b, c;
+    int a, b, c, y;
 	
-	printf("Vvedite a:");
-	scanf("%d", a);
-	printf("Vvedite b:");
-	scanf("%d", b);
-	printf("Vvedite c:");
-	scanf("%d", c);
+    printf("Vvedite a:");
+    scanf("%d", &a);
+    
+    printf("Vvedite b:");
+    scanf("%d", &b);
+    
+    printf("Vvedite c:");
+    scanf("%d", &c);
+    
+    y = max(a, b, c);
+
+    printf("%d", y);
 	
-	if ((a > b)&&(a > c)) {
-		printf("\nNaibolshee 4islo: %d", a);
-		} else {
-			if ((b > a)&&(b > c)) {
-				printf("\nNaibolshee 4islo: %d", b);
-			} else {
-				if ((c > a)&&(c > b)) {
-					printf("\nNaibolshee 4islo: %d", c);
-				} 
-		}
-	}
 	
 	return 0;
 }
