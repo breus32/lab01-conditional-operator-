267a3b5f4340832129e9938e3f4df42079ae0d38
breus32 <muller.09@mail.ru>
2016-05-07 20:19:06 +0300

    Доброго ранку, вечора, дня, ночі

--- zavdannya4.c
+++ zavdannya4.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 
-int main() {
-	
-	int a;
-	
-	printf("Vvedite vremya:");
-	scanf("%d", &a);
-	if ((a >= 5)&&(a <= 10)){
+int main() 
+{
+    int time;
+    
+    printf("Enter the time: ");
+    scanf("%d", &time);
+    	
+	if ((time >= 5)&&(time <= 10)){
 		printf("\nDobroe utro.");
 	} else {
-	if ((a >= 12)&&(a <= 18)) {
-		printf("\nDobriy den.");
-	} else {
-	if ((a >= 19)&&(a <= 24)){
-		printf("\nDobriy ve4er.");
-	} else {
-	if ((a >= 0)&&(a <= 5)) {
-		printf("\nSpokoinoy no4i.");
-				}
-			}
+	    if ((time >= 12)&&(time <= 18)) {
+		    printf("\nDobriy den.");
+	    } else {
+	        if ((time >= 19)&&(time <= 24)){
+		        printf("\nDobriy ve4er.");
+	        } else {
+	            if ((time >= 0)&&(time < 5)) {
+		            printf("\nSpokoinoy no4i.");
+		        } else {
+		            printf ("Error!");
+			   }
+		      }
 		}
 	}
 	return 0;
