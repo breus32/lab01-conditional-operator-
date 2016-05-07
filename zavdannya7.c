8452a15a4c0f75fe0c130951d5e5e165e2082273
breus32 <muller.09@mail.ru>
2016-05-07 20:39:55 +0300

    Високосний рік

--- zavdannya7.c
+++ zavdannya7.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 
-void leapyear (int b, int c, int d){
+void leapyear (int b, int c, int d, int a){
+	
+	b = a%4;
+	c = a%100;
+	d = a%400;
 	
 	if ((c == 0)&&(d != 0)) {
-		printf("\n0");
+		return 0;
 	} else {
 		if (b == 0) {
-			printf("\n1");
+			printf ("\n1");
+			return 1;
 		} else {
-			printf("\n0");
+			return 0;
 		}
 	}
 }
-
 
 int main ()
 {
@@ -20,12 +24,8 @@ int main ()
 	
 	printf("Vvedite god: ");
 	scanf("%d", &a);
+
 	
-	b = a%4;
-	c = a%100;
-	d = a%400;
-	
-	leapyear(b, c, d);
-		
+	leapyear(b, c, d, a);	
 	return 0;
 }
