66da4db7b715f1a72e3e0abd07d64cd018acd843
breus32 <muller.09@mail.ru>
2016-05-07 19:50:38 +0300

    0, або x, або pow (s, 3)

--- zavdannya3(2).c
+++ zavdannya3(2).c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 
-
-int main(){
-	int s, f;
-	
-	printf("Vvedite s:");
-	scanf ("%d", &s);
-	
-	if ((s > 0) && (s < 1)){
-		f = s;
-			printf ("f(s)= %d", f);
-	}else{
-			if ((s > 1)){
-				f = s * s * s;
-				printf ("f(s)= %d", f);
-			} else {
-				if (s <= 0) {
-					printf ("f(s)= 0");
-				}
-			}
-	}
+int main()
+{
+    int s, f;
+    
+    printf("Vvedite s:");
+    scanf ("%d", &s);
+    
+    if ((s > 0) && (s < 1)){
+    	f = s;
+    	printf ("f(s)= %d", f);
+    } else {
+    	if (s > 1){
+    	    f = s * s * s;
+    	    printf ("f(s)= %d", f);
+    	} else {
+    	    if (s <= 0) {
+    	    printf ("f(s)= 0");
+    	    }
+    	}
+    }
 	return 0;
 }
 
